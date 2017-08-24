a89fc528ea4f1f70f72767b499065957cfc427d7
Juan David Henao <jhenaoz93@gmail.com>
2017-08-23 23:09:54 -0500

    initial commit

---
+++ Assets/_Completed-Game/Scripts/Editor/PlayerTest.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEditor;
+using UnityEngine.TestTools;
+using NUnit.Framework;
+using System.Collections;
+
+public class PlayerTest {
+
+	[Test]
+	public void PlayerTestSimplePasses() {
+		Player player = new Player (12);
+
+		bool isWinner = player.isWinner ();
+
+		Assert.AreEqual (true, isWinner);
+
+	}
+
+	[Test]
+	public void PlayerTestSimpleFail() {
+		Player player = new Player (10);
+
+		bool isWinner = player.isWinner ();
+
+		Assert.AreEqual (false, isWinner);
+
+	}
+
+	[Test]
+	public void PlayerTestCountIncreasePass() {
+		Player player = new Player (11);
+		player.increaseCountBy (1);
+
+		bool isWinner = player.isWinner ();
+
+		Assert.AreEqual (true, isWinner);
+
+	}
+}
---
+++ Assets/_Completed-Game/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+public class Player
+{
+	private int _count;
+	private String _countText;
+	private int _maxCount = 12;
+
+	public Player (int count)
+	{
+		this._count = count;
+	}
+
+	public void increaseCountBy(int count){
+		this._count = this._count + count;
+	}
+
+	public String getCountText(){
+		return "Count: " + this._count.ToString ();
+	}
+
+	public bool isWinner() 
+	{
+		if (this._count >= this._maxCount) 
+		{
+			return true;
+		}
+		return false;
+	}
+
+
+}
